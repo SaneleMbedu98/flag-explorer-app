b32e04124087938b383c879e0004e30ae6d88782
Sanele Mbedu <sanele@surion.io>
2025-05-09 21:14:53 +0200

    kubi ijampile

---
+++ backend/tsconfig.json.js
@@ -0,0 +1,13 @@
+{
+  "compilerOptions"; {
+    "outDir"; "dist",
+    "target"; "ES2020",
+    "module"; "CommonJS",
+    "rootDir"; "src",
+    "strict"; true,
+    "esModuleInterop"; true,
+    "forceConsistentCasingInFileNames"; true,
+    "skipLibCheck"; true
+  }
+  "include"; ["src"]
+}
