77d23e30fd49a8bb0011b584c7f4f833712426f5
Sanele Mbedu <sanele@surion.io>
2025-05-13 20:10:45 +0200

    fixed connection refused

--- frontend/src/pages/Details.js
+++ frontend/src/pages/Details.js
@@ -14,7 +14,7 @@ function Details() {
     }
 
     //fetch(`https://flag-explorer-app.onrender.com/countries/${name}`)
-    fetch(`http://localhost:5000/country/${name}`)
+    fetch(`https://flag-explorer-app.onrender.com/country/${name}`)
       .then(response => {
         if (!response.ok) throw new Error('Country not found');
         return response.json();
